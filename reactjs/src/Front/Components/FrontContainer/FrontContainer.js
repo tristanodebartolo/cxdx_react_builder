7e600c251d81b3439939639243995cc80ed8a17c
Tristan De Bartolo <tristano.debartolo@almaviacx.com>
2023-04-03 00:06:34 +0200

    Create FrontContainer component

---
+++ reactjs/src/Front/Components/FrontContainer/FrontContainer.js
@@ -0,0 +1,9 @@
+import React, {useContext} from 'react'
+// Styles
+import './FrontContainer.scss'
+const FrontContainer = (props) => {
+  return (
+    <div className={`cxdx-wrapper`}>Hello World by Reactjs</div>
+  )
+}
+export default FrontContainer
